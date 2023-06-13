edcb5c969914c9b9ad9b2987958a411d3bbba2e1
aja_gorbataja <aja.gorbataja@yandex.ru>
2023-06-13 23:11:11 +0300

    порт 3001

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const router = require('./routes/routes');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 const handleErrors = require('./middlewares/handleErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
